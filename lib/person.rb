72375f1e773bf6410ca71dccb12f76cadd6198bd
Eric <eric.m.lombardo@gmail.com>
2020-12-09 09:15:01 -0500

    used metaprogramming to initialize a person with any number of arguements

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
-class Person
-  #your code here
+require 'pry'
+
+class Person  # creates a person
+  def initialize(attributes)  # receives any number of arguments in hash format
+    attributes.each do |key, value|   # loops through each key/ value pair
+      self.class.attr_accessor(key)   # creates an attr_accessor so it has set and get methods
+      self.send(("#{key}="), value)   # creates set method and assigns value for each key/value
+    end
+  end
 end
